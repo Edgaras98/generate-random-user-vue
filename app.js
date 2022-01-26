de3669b149f0abb166feaef0be973aeed01b815b
Edgaras98 <edgarasstepanovas14@@gmail.com>
2022-01-26 13:37:18 +0200

    Created generator tool.

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * Simple random user generator using randomuser.me/api
+ */
+
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/med/men/75.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.medium;
+    },
+  },
+});
+
+app.mount("#app");
